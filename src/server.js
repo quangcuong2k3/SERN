61fd9c57ca1b1f64cdf4d40fc389cb9afd2e923f
Cuong <cuongquangle539@gmail.com>
2025-02-07 07:58:27 +0700

    video 39

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebroutes from "./route/web";
+import connectDB from "./config/connectDB";
+import cors from 'cors';
+
+require('dotenv').config();
+
+
+let app = express();
+app.use(cors({ credentials: true, origin: true }));
+//config app
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebroutes(app);
+
+connectDB();
+
+let port = process.env.PORT || 6969;
+app.listen(port, () => {
+    //callback
+    console.log("backend nodejs running on the port :" + port)
+})
