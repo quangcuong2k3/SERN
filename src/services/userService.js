7b8e848b03fce06d13d815e645f7081400eeee90
Cuong <cuongquangle539@gmail.com>
2025-02-08 22:09:07 +0700

    video 41

--- src/services/userService.js
+++ src/services/userService.js
@@ -110,22 +110,24 @@ let createNewUser = (data) => {
                     errCode: 1,
                     errMessage: "Your email is already in used, Pls try another email!!"
                 })
+            } else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender === "1" ? true : false,
+                    roleId: data.roleId,
+                })
+                resolve({
+                    errCode: 0,
+                    errMessage: "ok"
+                })
             }
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phonenumber: data.phonenumber,
-                gender: data.gender === "1" ? true : false,
-                roleId: data.roleId,
-            })
-            resolve({
-                errCode: 0,
-                errMessage: "ok"
-            });
+
         } catch (e) {
             reject(e)
         }
