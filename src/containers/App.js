fdbd0577adc48223deb8a7ede95abe2b373d6f44
Cuong <cuongquangle539@gmail.com>
2025-02-17 08:58:20 +0700

    video 48

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,6 +13,7 @@ import Header from './Header/Header';
 import System from '../routes/System';
 import { CustomToastCloseButton } from '../components/CustomToast';
 import HomePage from './HomePage/HomePage.js'
+import CustomScrollbars from '../components/CustomScrollbars.js';
 
 class App extends Component {
 
@@ -40,15 +41,16 @@ class App extends Component {
                 <Router history={history}>
                     <div className="main-container">
                         {this.props.isLoggedIn && <Header />}
-                        <span className="content-container">
-                            <Switch>
-                                <Route path={path.HOME} exact component={(Home)} />
-                                <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
-                                <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
-                                <Route path={path.HOMEPAGE} component={HomePage} />
-
-                            </Switch>
-                        </span>
+                        <div className="content-container">
+                            <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
+                                <Switch>
+                                    <Route path={path.HOME} exact component={(Home)} />
+                                    <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
+                                    <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                    <Route path={path.HOMEPAGE} component={HomePage} />
+                                </Switch>
+                            </CustomScrollbars>
+                        </div>
 
                         <ToastContainer
                             className="toast-container" toastClassName="toast-item" bodyClassName="toast-item-body"
--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,10 +3,16 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import logo from "../../assets/logo.png";
 import { FormattedMessage } from 'react-intl';
+import { LANGUAGES } from '../../utils';
+import { changeLanguageApp } from '../../store/actions'
 
 class HomeHeader extends Component {
-
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language)
+    }
     render() {
+        let language = this.props.language;
+        console.log("check laguage", language)
         return (
             <React.Fragment>
                 <div className='home-header-container'>
@@ -35,8 +41,18 @@ class HomeHeader extends Component {
                         </div>
                         <div className='right-content'>
                             <div className='support'><i className="fas fa-question-circle"></i><FormattedMessage id="homeheader.support" /></div>
-                            <div className='language-vi'>VN</div>
-                            <div className='language-en'>EN</div>
+                            <div
+                                className={language === LANGUAGES.VI ?
+                                    'language-vi active' : 'language-vi'}>
+                                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                                    VN
+                                </span></div>
+                            <div
+                                className={language === LANGUAGES.EN ?
+                                    'language-en active' : 'language-en'}>
+                                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                                    EN
+                                </span></div>
 
                         </div>
                     </div>
@@ -94,6 +110,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
