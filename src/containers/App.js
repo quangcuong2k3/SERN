f91cc7a99a563c60a0db5f87a251502be901c388
Cuong <cuongquangle539@gmail.com>
2025-02-20 20:49:13 +0700

    video 52

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,6 @@ import { ToastContainer } from 'react-toastify';
 import { userIsAuthenticated, userIsNotAuthenticated } from '../hoc/authentication';
 import { path } from '../utils'
 import Home from '../routes/Home';
-// import Login from '../routes/Login';
 import Login from './Auth/Login';
 import Header from './Header/Header';
 import System from '../routes/System';
@@ -40,7 +39,6 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        {this.props.isLoggedIn && <Header />}
                         <div className="content-container">
                             <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
                                 <Switch>
