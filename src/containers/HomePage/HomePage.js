a3cea0bff9f0958b96bb3ef828c23a0b2d0c7a34
Cuong <cuongquangle539@gmail.com>
2025-02-20 10:01:55 +0700

    video 51

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -2,15 +2,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 import Specialty from './Section/Specialty';
+import MedicalFacility from './Section/MedicalFacility';
+import OutstandingDoctor from './Section/OutstandingDoctor';
+import HandBook from './Section/HandBook';
+import About from './Section/About';
+import HomeFooter from './HomeFooter';
+import './HomePage.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 class HomePage extends Component {
 
     render() {
+        let settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4, //cho xem bao nhieu anh dong
+            slidesToScroll: 1,
+        };
         return (
             <div>
                 <HomeHeader />
-                <Specialty />
-                <div style={{ height: '300px' }}></div>
+                <Specialty
+                    settings={settings} />
+                <MedicalFacility
+                    settings={settings} />
+                <OutstandingDoctor
+                    settings={settings} />
+                <HandBook
+                    settings={settings} />
+                <About
+                    settings={settings} />
+                <HomeFooter
+                    settings={settings} />
             </div>
         );
     }
---
+++ src/containers/HomePage/Section/About.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+
+class About extends Component {
+
+    render() {
+
+        return (
+            <div className='section-share section-about'>
+                <div className='section-about-header'>
+                    Truyền thông nói về BookingCare
+                </div>
+                <div className='section-about-content'>
+                    <div className='content-left'>
+                        <iframe width="100%" height="400px"
+                            src="https://www.youtube.com/embed/FyDQljKtWnI"
+                            title="CÀ PHÊ KHỞI NGHIỆP VTV1 - BOOKINGCARE - HỆ THỐNG ĐẶT LỊCH KHÁM TRỰC TUYẾN"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerpolicy="strict-origin-when-cross-origin"
+                            allowfullscreen></iframe>
+                    </div>
+                    <div className='content-right'>
+                        <p>
+                            Các bạn có thể làm chủ công nghệ, cũng như học được, biết được những kiến thức thực tế dùng tại các công ty hiện nay. Sau khi kết thúc khóa học này, mình tin chắc rằng dự án này đủ lớn,
+                            đủ thực tế để cho các bạn mới ra trường viết vào CV xin việc của mình ^^
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
