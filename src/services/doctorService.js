792b745434173a2c83275fb8d2aa1aadd01ec5e6
Cuong <cuongquangle539@gmail.com>
2025-03-23 09:31:10 +0700

    80

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,6 +1,6 @@
 import db from "../models/index"
 require('dotenv').config();
-import _ from 'lodash';
+import _, { includes } from 'lodash';
 
 const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 let getTopDoctorHome = (limitInput) => {
@@ -150,6 +150,18 @@ let getDetailDoctorById = (inputId) => {
                             attributes: ['description', 'contentHTML', 'contentMarkdown']
                         },
                         { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+                        {
+                            model: db.Doctor_Infor,
+                            attributes: {
+                                exclude: ['id', 'doctorId']
+                            },
+                            includes: [
+                                { model: db.Allcode, as: 'priceTypeData', attributes: ['valueEn', 'valueVi'] },
+                                { model: db.Allcode, as: 'provinceTypeData', attributes: ['valueEn', 'valueVi'] },
+                                { model: db.Allcode, as: 'paymentTypeData', attributes: ['valueEn', 'valueVi'] },
+
+                            ]
+                        },
                     ],
                     raw: false,
                     nest: true
@@ -158,6 +170,7 @@ let getDetailDoctorById = (inputId) => {
                     data.image = new Buffer(data.image, 'base64').toString('binary')
                 }
                 if (!data) data = {};
+
                 resolve({
                     errCode: 0,
                     data: data
