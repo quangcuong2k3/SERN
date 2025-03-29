0634cece2e1cb9e58b3698843520fadfb6ad6129
Cuong <cuongquangle539@gmail.com>
2025-03-29 14:20:22 +0700

    94

--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -1,6 +1,7 @@
 const { reject } = require("lodash")
 const db = require("../models")
 
+
 let createSpecialty = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -48,7 +49,54 @@ let getAllSpecialty = () => {
         }
     })
 }
+let getDetailSpecialtyById = (inputId, location) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId || !location) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing parament!"
+                })
+            } else {
+                let data = await db.Specialty.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: ['descriptionHTML', 'descriptionMarkdown']
+                })
+                if (data) {
+                    let doctorSpecialty = [];
+                    if (location === 'ALL') {
+                        doctorSpecialty = await db.Doctor_Infor.findAll({
+                            where: { specialtyId: inputId },
+                            attributes: ['doctorId', 'provinceId']
+                        })
+                    } else {
+                        //find by location
+                        doctorSpecialty = await db.Doctor_Infor.findAll({
+                            where: {
+                                specialtyId: inputId,
+                                provinceId: location
+                            },
+                            attributes: ['doctorId', 'provinceId']
+                        })
+                    }
+                    data.doctorSpecialty = doctorSpecialty;
+
+                } else data = {}
+                resolve({
+                    errMessage: 'OK',
+                    errCode: 0,
+                    data
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
 module.exports = {
     createSpecialty: createSpecialty,
-    getAllSpecialty: getAllSpecialty
+    getAllSpecialty: getAllSpecialty,
+    getDetailSpecialtyById: getDetailSpecialtyById
 }
