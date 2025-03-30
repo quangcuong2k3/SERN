5b4e77c3865c2b64e781ee7f8aaa1d051e5ef0e8
Cuong <cuongquangle539@gmail.com>
2025-03-30 21:09:02 +0700

    97

--- src/services/clinicService.js
+++ src/services/clinicService.js
@@ -1,3 +1,4 @@
+const { reject } = require("lodash")
 const db = require("../models")
 
 let createClinic = (data) => {
@@ -29,6 +30,66 @@ let createClinic = (data) => {
         }
     })
 }
+let getAllClinic = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let data = await db.Clinic.findAll({
+
+            });
+            if (data && data.length > 0) {
+                data.map(item => {
+                    item.image = new Buffer(item.image, 'base64').toString('binary');
+                    return item;
+                })
+            }
+            resolve({
+                errCode: 0,
+                errMessage: 'ok',
+                data
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+let getDetailClinicById = (inputId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing parament!"
+                })
+            } else {
+                let data = await db.Clinic.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: ['descriptionHTML', 'descriptionMarkdown', 'address', 'name']
+                })
+                if (data) {
+                    let doctorClinic = [];
+                    doctorClinic = await db.Doctor_Infor.findAll({
+                        where: { clinicId: inputId },
+                        attributes: ['doctorId', 'provinceId']
+                    })
+                    data.doctorClinic = doctorClinic;
+
+                } else data = {}
+                resolve({
+                    errMessage: 'OK',
+                    errCode: 0,
+                    data
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
-    createClinic: createClinic
+    createClinic: createClinic,
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById
 }
