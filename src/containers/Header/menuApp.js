6d9be03b1bf626eeb7d473694c9d17ecaa9b1305
Cuong <cuongquangle539@gmail.com>
2025-03-31 12:37:38 +0700

    98

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -56,8 +56,10 @@ export const doctorMenu = [
         name: 'menu.admin.manage-user',
         menus: [
             { //Quản lý kế hoạch khám bệnh của bác sĩ
-
                 name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+            },
+            { //Quản lý bệnh nhân khám bệnh của bác sĩ
+                name: 'menu.doctor.manage-patient', link: '/doctor/manage-patient'
             },
         ]
     }
