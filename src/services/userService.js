bfa6f95c5c2ad89f8544e25430ff8e8927623852
Cuong <cuongquangle539@gmail.com>
2025-04-01 19:04:36 +0700

    video 100

--- src/services/userService.js
+++ src/services/userService.js
@@ -78,6 +78,10 @@ const createNewClinic = (data) => {
 const getAllPatientForDoctor = (data) => {
     return axios.get(`/api/get-list-patient-for-doctor?doctorId=${data.doctorId}&date=${data.date}`)
 }
+const postSendRemedy = (data) => {
+    return axios.post('/api/send-remedy', data)
+}
+
 export {
     handleLoginApi, getAllUsers,
     createNewUserService, deleteUserService,
@@ -90,6 +94,6 @@ export {
     postVerifyBookingAppointment, createNewSpecialty,
     getAllSpecialty, getAllDetailSpecialtyById,
     createNewClinic, getAllClinic, getAllDetailClinicById,
-    getAllPatientForDoctor
+    getAllPatientForDoctor, postSendRemedy
 
 }
